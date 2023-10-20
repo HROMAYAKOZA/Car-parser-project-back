ab4474bbb97762a8764069e6eb1ed7be4d390a19
sultan <sasha530000@gmail.com>
2023-10-20 14:15:23 +0300

    Output from DB and add a DB update function

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from services import app, scheduler, db
 from services.advertisement import updatingADS
 
-scheduler.add_job(id='Task of updating the database', func=updatingADS, trigger="interval", seconds=500)
+scheduler.add_job(id='Task of updating the database', func=updatingADS, trigger="interval", seconds=3600)
 scheduler.start()
 
 app.run(debug=True, use_reloader=False)
