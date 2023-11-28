3749b699e24024ca1b7be02ce5973714b1552d75
haribo0372 <sasha530000@gmail.com>
2023-11-28 13:07:42 +0300

    big fix

--- services/routes.py
+++ services/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 
@@ -8,7 +8,7 @@ from services.href import cities
 from services.advertisement import sorted_selectFromADS, \
     insert_ad_to_Advertisement
 
-insert_ad_to_Advertisement(cities, 10)
+# insert_ad_to_Advertisement(cities, 10)
 cities_db = []
 brands = []
 models = []
@@ -26,16 +26,21 @@ for ad in Advertisement.query.all():
     if not (ad.transmission in transmissions):
         transmissions.append(ad.transmission)
 
+InfoLists = {"cities": cities_db, "brands": brands, "models": models,
+             "years" : years, "transmissions": transmissions, "lens" : 5}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
-        ads = Advertisement.query.all()
-        return render_template('index.html',
-                               ads=ads,
-                               len=5, brands=brands,
-                               models=models, cities=cities_db,
-                               trans=transmissions, years=years)
+        ads = Advertisement.query.limit(10).all()
+        InfoLists["ads"] = [{'id': item.id,
+                             'brand': item.brand,
+                             'model' : item.model,
+                             'year' : item.year,
+                             'price' : item.price,
+                             'image' : item.img_url} for item in ads]
+        return jsonify(InfoLists)
     elif request.method == 'POST':
         brand = request.form.get('brand')
         model = request.form.get('model')
@@ -44,12 +49,11 @@ def main():
         trans = request.form.get('transmission')
         price_from = request.form.get('price_from')
         price_to = request.form.get('price_to')
-        ads = sorted_selectFromADS(brand, model, city, price_from, price_to)
-        return render_template('index.html',
-                               ads=ads,
-                               len=len(ads), brands=brands,
-                               models=models, cities=cities_db,
-                               trans=transmissions, years=years)
+        ads = sorted_selectFromADS(brand, model, city,
+                                   price_from, price_to)
+        InfoLists["ads"] = ads
+        InfoLists["lens"] = len(ads)
+        return InfoLists
 
 
 @app.route('/advertisement/<ID>', methods=['GET', 'POST'])
@@ -59,58 +63,58 @@ def car_page(ID):
 
 
 @app.route('/login', methods=['POST', 'GET'])
-def login_page():
+def login_page() -> dict:
+    """Returns a dictionary with an error message and the username of the user.\n
+     If the error message is empty in the dictionary,then the user is transferred to his account"""
+    result = {'message': '', 'username': ''}
     login = request.form.get('login')
     password = request.form.get('password')
-    if request.method == 'POST':
-        if login and password:
-            user = Users.query.filter_by(login=login).first()
-            if user and check_password_hash(user.password, password):
-                login_user(user)
-
-                next_page = request.args.get('next')
-                if next_page:
-                    return redirect(next_page)
-                else:
-                    return redirect(
-                        url_for("user_page", username=current_user.nickname))
-            else:
-                flash("Неверный логин или пароль")
-
+    if login and password:
+        user = Users.query.filter_by(login=login).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            print("login")
+            result['username'] = current_user.nickname
+            return result
         else:
-            flash("Пожалуйста, введите логин и пароль")
-
-    return render_template('login.html')
+            result['message'] = "Неверный логин или пароль"
+    else:
+        result['message'] = "Пожалуйста, введите логин и пароль"
+    print("Login: {0}  {1}".format(login, password))
+    return result
 
 
 @app.route('/registration', methods=['POST', 'GET'])
-def registration():
+def registration() -> dict:
+    """Returns a **dictionary** with an **error message** and the **username** of the user.\n
+    If the error message **is empty** in the dictionary, then the user is **successfully registered**"""
+    result = {'message': '', 'username': ''}
     nickname = request.form.get('nickname')
     login = request.form.get('login')
     password = request.form.get('password')
     password2 = request.form.get('password2')
-    if request.method == 'POST':
-        if not (password or password2 or login or nickname):
-            flash("Пожалуйста, заполните все поля")
-        elif password != password2:
-            flash("Пароли не совпадают")
+
+    if not (password or password2 or login or nickname):
+        result['message'] = "Пожалуйста, заполните все поля"
+    elif password != password2:
+        result['message'] = "Пароли не совпадают"
+    else:
+        if ' ' in list(login):
+            result['message'] = "Логин не должен содержать пробела"
         else:
-            if ' ' in list(login):
-                flash("Логин не должен содержать пробела")
+            user = Users.query.filter_by(login=login).first()
+            if user:
+                result['message'] = "Этот логин уже занят"
             else:
-                user = Users.query.filter_by(login=login).first()
-                if user:
-                    flash("Этот логин уже занят")
-                else:
-                    hash_password = generate_password_hash(password)
-                    newUser = Users(nickname=nickname, login=login,
-                                    password=hash_password)
-                    db.session.add(newUser)
-                    db.session.commit()
+                result['username'] = nickname
+                hash_password = generate_password_hash(password)
+                newUser = Users(nickname=nickname, login=login,
+                                password=hash_password)
+                db.session.add(newUser)
+                db.session.commit()
 
-                    return redirect(url_for('login_page'))
-
-    return render_template('registration.html')
+    print("{2} : {0}  {1}".format(login, password, nickname))
+    return result
 
 
 @app.route('/logout', methods=['POST', 'GET'])
@@ -120,14 +124,22 @@ def logout():
     return redirect(url_for('main'))
 
 
-@app.after_request
-def redirect_to_signin(response):
-    if response.status_code == 401:
-        return redirect(url_for('login_page') + '?next=' + request.url)
-    return response
+# @app.after_request
+# def redirect_to_signin(response):
+#     if response.status_code == 401:
+#         return redirect(url_for('login_page') + '?next=' + request.url)
+#     return response
 
 
-@app.route('/account/<username>', methods=['GET'])
-@login_required
-def user_page(username):
-    return render_template('account.html')
+# @app.route('/account/<username>', methods=['GET'])
+# @login_required
+# def user_page(username):
+#     return render_template('account.html')
+
+@app.route('/account', methods=['GET'])
+def user_page() -> dict:
+    """Returns a dictionary in which the **key is a parameter**,
+    and the **value is a list** from which you can select a variant of this parameter"""
+    name = current_user.nickname
+    info = {"username": name}
+    return info
